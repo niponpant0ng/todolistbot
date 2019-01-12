75a010af6e71db69f7ba04778188304897921393
nipon pantong <nipon.pantong@scb.co.th>
2019-01-12 12:47:21 +0700

    init project

---
+++ src/main/java/com/example/todolist/TodolistController.java
@@ -0,0 +1,24 @@
+package com.example.todolist;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+@LineMessageHandler
+public class TodolistController {
+    @EventMapping
+    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        System.out.println("event: " + event);
+        final String originalMessageText = event.getMessage().getText();
+        return new TextMessage(originalMessageText);
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+    }
+}
---
+++ src/main/java/com/example/todolist/service/TodoListService.java
@@ -0,0 +1,7 @@
+package com.example.todolist.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoListService {
+}
